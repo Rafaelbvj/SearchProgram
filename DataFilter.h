288ea1fe94c574b4048c2e5ffd44acf69c69fbab
Rafael Costa <Rafaelbvj@gmail.com>
2016-11-21 18:36:15 -0200

    Update DataFilter.h

--- DataFilter.h
+++ DataFilter.h
@@ -5,26 +5,26 @@
 #define  FILEERROR              1
 #define  OPTIONERROR            2
 
-#define  FILTER_ALL             0
-#define  FILTER_GMC             1
-#define  FILTER_GMF             3
-#define  FILTER_EMP             4
-#define  FILTER_DPLC            5
-#define  FILTER_DPLF            6
-#define  FILTER_FORA            7
-#define  FILTER_FORAMARCA       8
-#define  FILTER_FORANAOMARCA    9
-#define  FILTER_CASA            0xA
-#define  FILTER_CASAMARCA       0xB
-#define  FILTER_CASANAOMARCA    0xC
-#define  FILTER_MENOS1          0xD
-#define  FILTER_MENOS2          0xE
-#define  FILTER_MENOS3          0xF
-#define  FILTER_MENOS4          0x10
-#define  FILTER_MAIS1           0x11
-#define  FILTER_MAIS2           0x12
-#define  FILTER_MAIS3           0x13
-#define  FILTER_MAIS4           0x14
+#define  FILTER_ALL             0x13
+#define  FILTER_GMC             0
+#define  FILTER_GMF             1
+#define  FILTER_EMP             2
+#define  FILTER_DPLC            3
+#define  FILTER_DPLF            4
+#define  FILTER_FORA            5
+#define  FILTER_FORAMARCA       6
+#define  FILTER_FORANAOMARCA    7
+#define  FILTER_CASA            8
+#define  FILTER_CASAMARCA       9
+#define  FILTER_CASANAOMARCA    0xA
+#define  FILTER_MENOS1          0xB
+#define  FILTER_MENOS2          0xC
+#define  FILTER_MENOS3          0xD
+#define  FILTER_MENOS4          0xE
+#define  FILTER_MAIS1           0xF
+#define  FILTER_MAIS2           0x10
+#define  FILTER_MAIS3           0x11
+#define  FILTER_MAIS4           0x12
 typedef struct dadosaposta{
 //Tabela inicial
 float GMC;  float GMF;     float DPLC;         float DPLF;
@@ -45,6 +45,6 @@ int sizeData;
 
 int GetBetData(PDadosAposta,char*);
 int FilterBetData(PDadosAposta,const int);
-
+void ReleaseBetData(PDadosAposta);
 
 
